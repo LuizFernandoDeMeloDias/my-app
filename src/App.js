a4714df48960e59f3a2c054b41c4faa6109ffc5c
LuizFernandodeMeloDias <valitio999@gmail.com>
2023-12-24 09:08:21 -0300

    Estudo React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
+
+import React, {useState} from 'react';
 import './App.css';
 import TodoTable from './componentes/TodoTable';
 
 
 function App() {
 
-const fazeres = [
+const [fazeres, setFazeres] = useState([
   {rowNumber: 1, rowDescription: 'Alimentar o Gato', rowDesignado: 'Lulu'},
   {rowNumber: 2, rowDescription: 'Regar o cacto', rowDesignado: 'EUa'},
   {rowNumber: 3, rowDescription: 'Matar alguém', rowDesignado: 'EU'},
   {rowNumber: 4, rowDescription: 'Matar alguém', rowDesignado: 'EU'},
   {rowNumber: 5, rowDescription: 'Matar alguém', rowDesignado: 'EU'},
-]
+])
 
-  const Addtodo = () => {
-    if (fazeres > 0){
-      const NewTodo = {
+  const addTodo = () => {
+    if (fazeres.length > 0){
+      const newTodo = {
         rowNumber: fazeres.length + 1,
         rowDescription: 'new todo',
         rowDesignado: 'new user'
       }
-      fazeres.push( NewTodo);
-
+      setFazeres(fazeres => [...fazeres, newTodo]);
     }
   }
 
@@ -32,7 +33,7 @@ const fazeres = [
         </div>
         <div className='card-body'>
           <TodoTable fazeres={fazeres}/>
-          <button className='btn btn-primary' onClick={Addtodo}>Add new todo</button>
+          <button className='btn btn-primary' onClick={addTodo}>Add new todo</button>
         </div>
       </div>
     </div>
